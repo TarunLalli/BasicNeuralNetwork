d3d9528a8466e8892681b507cb752301412fa6ff
TarunLalli <tarunslalli@outlook.com>
2025-06-08 22:11:33 +0100

    Setting up intial structure

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 import numpy as np
 import math
+
+
+def main(dataset):
+    ...
+
+
+class Neural_Network:
+    def __init__(self):
+        ...
+    
+    def sigmoid(x):
+        return(1/(1+math.exp(-x)))
+
+    def GradientDescent():
+        ...
+
+    def ForwardPropagation():
+        ...
+
+    def softmaxfunct():
+        ...
+
+    def neuron():
+        ...
