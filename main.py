adaa0d681ac1609e382eac5449cd538f2088e8b0
TarunLalli <tarunslalli@outlook.com>
2025-06-10 20:27:50 +0100

    Adding simple maths functions

--- main.py
+++ main.py
@@ -7,11 +7,24 @@ def main(dataset):
 
 
 class Neural_Network:
-    def __init__(self):
-        ...
+    def __init__(self , input):
+        self.input = input
     
+    # Mathematical Formulae
     def sigmoid(x):
         return(1/(1+math.exp(-x)))
+
+    def ReLU(x):
+        if x>=0:
+            return x
+        
+    def softmaxfunct(z):
+        sigma_z = []
+        for z_i in z:
+            sigma_z.append(
+                math.exp(z_i)/(math.exp(z))
+            )
+        return sigma_z
 
     def GradientDescent():
         ...
@@ -19,8 +32,20 @@ class Neural_Network:
     def ForwardPropagation():
         ...
 
-    def softmaxfunct():
-        ...
+    
 
     def neuron():
         ...
+
+
+
+def dummy(z):
+    sigma_z = []
+    for z_i in z:
+        sigma_z.append(
+            math.exp(z_i)/(math.exp(z))
+        )
+
+dummy(
+    [1,2,3,4,5]
+)
